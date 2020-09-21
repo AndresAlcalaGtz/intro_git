9fc88430a5f557257e96b0e518d177f4e965a9ce
Andres Alcala <hector.alcala2936@alumnos.udg.mx>
2020-09-20 19:49:51 -0500

    Capturamos el nombre, la edad y le decimos su edad+1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("Hola mundo!")
 
 nombre = input("Nombre: ")
-edad = input("Edad: ")
+edad = int(input("Edad: "))
 print(f"Hola {nombre} de {edad} años")
+print(f"Vas a cumplir {edad + 1} años")
